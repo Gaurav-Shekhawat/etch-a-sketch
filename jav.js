1830e5dc5fcb836fdbdf4f95ecca8113083794da
Gaurav-Shekhawat <gaurav_s@cs.iitr.ac.in>
2021-08-27 22:27:18 +0530

    removed consolelog and commented greyscale

--- jav.js
+++ jav.js
@@ -22,7 +22,6 @@ function getRandomColor() {
 }
 
 function colorSchemeNewDivs(color_value) {
-  console.log("hi");
   const new_divs = canvas.querySelectorAll(".new_divs");
 
   new_divs.forEach((new_div) => {
@@ -50,31 +49,31 @@ function eraseNow() {
   colorSchemeNewDivs("white");
 }
 
-function greyShade() {
-  if (this.style.backgroundColor === "rgb(0, 0, 0)") {
-    return;
-  }
+// function greyShade() {
+//   if (this.style.backgroundColor === "rgb(0, 0, 0)") {
+//     return;
+//   }
 
-  let mystring = this.style.backgroundColor;
-  let length = mystring.length;
-  let new_string = mystring.substring(4, length - 1);
-  let faaltu_array = new_string.split(", ");
+//   let mystring = this.style.backgroundColor;
+//   let length = mystring.length;
+//   let new_string = mystring.substring(4, length - 1);
+//   let faaltu_array = new_string.split(", ");
 
-  this.style.backgroundColor = `rgb(${Number(faaltu_array[0]) - 25.5},${
-    Number(faaltu_array[0]) - 25.5
-  }, ${Number(faaltu_array[0]) - 25.5})`;
+//   this.style.backgroundColor = `rgb(${Number(faaltu_array[0]) - 25.5},${
+//     Number(faaltu_array[0]) - 25.5
+//   }, ${Number(faaltu_array[0]) - 25.5})`;
 
-  console.log(this.style.backgroundColor);
-}
+//   console.log(this.style.backgroundColor);
+// }
 
-function greyFunction() {
-  const new_divs = canvas.querySelectorAll(".new_divs");
+// function greyFunction() {
+//   const new_divs = canvas.querySelectorAll(".new_divs");
 
-  console.log(this);
-  new_divs.forEach((new_div) => {
-    new_div.addEventListener("mouseover", greyShade);
-  });
-}
+//   console.log(this);
+//   new_divs.forEach((new_div) => {
+//     new_div.addEventListener("mouseover", greyShade);
+//   });
+// }
 
 function canvas_designer(number_of_boxes) {
   clearcanvas();
@@ -92,7 +91,6 @@ function canvas_designer(number_of_boxes) {
   }
   color_picker.value = "#000000";
   const new_divs = canvas.querySelectorAll(".new_divs");
-  console.log("am i running");
 
   new_divs.forEach((new_div) => {
     new_div.addEventListener("mouseover", function () {
@@ -122,28 +120,28 @@ slider.setAttribute("oninput", "sliderchange()");
 
 canvas_designer(number_of_boxes);
 
-function startGrey() {
-  const new_divs = canvas.querySelectorAll(".new_divs");
+// function startGrey() {
+//   const new_divs = canvas.querySelectorAll(".new_divs");
 
-  new_divs.forEach((new_div) => {
-    new_div.addEventListener(
-      "mouseover",
-      function () {
-        new_div.style.backgroundColor = "rgb(255, 255, 255)";
-      },
-      false
-    );
-  });
+//   new_divs.forEach((new_div) => {
+//     new_div.addEventListener(
+//       "mouseover",
+//       function () {
+//         new_div.style.backgroundColor = "rgb(255, 255, 255)";
+//       },
+//       false
+//     );
+//   });
 
-  greyFunction();
-}
+//   greyFunction();
+// }
 
 let blackWhite = document.querySelector(".black-white");
-let greyScale = document.querySelector(".grey-scale");
+//let greyScale = document.querySelector(".grey-scale");
 let rainbow = document.querySelector(".rainbow");
 let eraser = document.querySelector(".eraser");
 
 blackWhite.addEventListener("click", blackCOLOR);
 rainbow.addEventListener("click", randomRGB);
 eraser.addEventListener("click", eraseNow);
-greyScale.addEventListener("click", startGrey);
+//greyScale.addEventListener("click", startGrey);
